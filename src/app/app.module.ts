b5003aa727ccc0f92487a027065628d266ccbf90
Kheronn <kheronn@gmail.com>
2018-09-03 11:59:17 -0300

    Setup, home, menu and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './public/home/home.component';
+import { MenuPrincipalComponent } from './public/menu-principal/menu-principal.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuPrincipalComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
